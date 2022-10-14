7d3b318a5d14ba85410f88d224f8838776dfc883
rgchang <roger5521610@gmail.com>
2022-10-14 22:18:51 +0800

    [FIX] fix download.sh tslot_tag.sh bug

--- hw1/slot_tag.sh
+++ hw1/slot_tag.sh
@@ -1,4 +1,4 @@
 # "${1}" is the first argument passed to the script
 # "${2}" is the second argument passed to the script
-python3 test_slot.py --data_dir "${1}" --ckpt_dir hw1_2_model/hw1_2_15_js_0.800.pt --pred_dir "${2}"
+python3 test_slot.py --data_dir "${1}" --ckpt_dir model/hw1_2_model.pt --pred_dir "${2}"
 # python3 test_slot.py --data_dir "${1}" --ckpt_dir hw1_2_model/hw1_2_30_js_0.774.pt --pred_dir "${2}"
