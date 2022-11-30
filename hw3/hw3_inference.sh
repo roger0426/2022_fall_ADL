c77baa6d6c9499c0b2290a5259a50ce2c3a6e9a0
rgchang <roger5521610@gmail.com>
2022-11-30 14:55:34 +0800

    [TASK] Report.pdf Done, fix some bugs

--- hw3/hw3_inference.sh
+++ hw3/hw3_inference.sh
@@ -2,43 +2,43 @@
 
 echo ==========Beam = 4==========
 python3.8 ./huggingface_code/run_summarization_inference.py \
-    --model_name_or_path ./checkpoint_8_b5/epoch_9 \
+    --model_name_or_path ./checkpoint_8_b4/epoch_26 \
     --test_file ./hw3_data/public.jsonl \
     --output_dir ./output/ \
+    --output_file ./pred.jsonl \
     --text_column maintext \
     --preprocessing_num_workers 10 \
     --per_device_test_batch_size 8 \
     --num_beams 4 \
-    --with_tracking \
     # 2> /dev/null
-python3.8 eval.py -r hw3_data/public.jsonl -s output/predicts_big5.jsonl 2> /dev/null
+python3.8 eval.py -r hw3_data/public.jsonl -s ./pred.jsonl 2> /dev/null
 
 echo ==========Beam = 16==========
 python3.8 ./huggingface_code/run_summarization_inference.py \
-    --model_name_or_path ./checkpoint_8_b5/epoch_9 \
+    --model_name_or_path ./checkpoint_8_b4/epoch_26 \
     --test_file ./hw3_data/public.jsonl \
     --output_dir ./output/ \
+    --output_file ./pred.jsonl \
     --text_column maintext \
     --preprocessing_num_workers 10 \
     --per_device_test_batch_size 8 \
     --num_beams 16 \
-    --with_tracking \
     # 2> /dev/null
-python3.8 eval.py -r hw3_data/public.jsonl -s output/predicts_big5.jsonl 2> /dev/null
+python3.8 eval.py -r hw3_data/public.jsonl -s ./pred.jsonl 2> /dev/null
 
 
-echo ==========Beam = 128==========
+echo ==========Beam = 64==========
 python3.8 ./huggingface_code/run_summarization_inference.py \
-    --model_name_or_path ./checkpoint_8_b5/epoch_9 \
+    --model_name_or_path ./checkpoint_8_b4/epoch_26 \
     --test_file ./hw3_data/public.jsonl \
     --output_dir ./output/ \
+    --output_file ./pred.jsonl \
     --text_column maintext \
     --preprocessing_num_workers 10 \
-    --per_device_test_batch_size 2 \
-    --num_beams 128 \
-    --with_tracking \
+    --per_device_test_batch_size 1 \
+    --num_beams 64 \
     # 2> /dev/null
-python3.8 eval.py -r hw3_data/public.jsonl -s output/predicts_big5.jsonl 2> /dev/null
+python3.8 eval.py -r hw3_data/public.jsonl -s ./pred.jsonl 2> /dev/null
 
 # echo ==========top_k = 2==========
 # python3.8 ./huggingface_code/run_summarization_inference.py \
